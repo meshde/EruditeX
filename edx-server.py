ff1f0e4728277a522303c8a3304383eded66df49
Jerin John <jerinjohn101@gmail.com>
2017-09-09 16:16:51 +0530

    Server Code updated and tested.

--- edx-server.py
+++ edx-server.py
@@ -1,14 +1,20 @@
 import subprocess
 from flask import Flask
+from flask import Response
 from flask import request
+import json
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def get_passage():
-	query = request.form['query']
+	query = request.get_json(force=True)['query']
+	print(query)
+#  	resp = Response(query)
 	proc = subprocess.Popen(['python','IR/InfoRet.py',query],shell=False,stdout=subprocess.PIPE)
-	return proc.communicate()[0].decode()
+	resp = Response(proc.communicate()[0].decode())
+	resp.headers['Access-Control-Allow-Origin'] = '*'
+	return resp
 
 if __name__ == '__main__':
 	app.run()
