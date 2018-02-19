216764da7c7b0529797950ade552326dbe89ec4f
meshde <meshde.md@gmail.com>
2018-02-19 10:23:47 +0530

    Remove Siblings of Answer Node

--- Models/ans_token.py
+++ Models/ans_token.py
@@ -16,7 +16,6 @@ class AnsSelect(object):
         ans_sent_hid = T.vector("Answer root node hidden state")
         ans_node_hid = T.vector("Answer word node hidden state")
         ans_parent_hid = T.vector("Answer word's parent hidden state")
-        ans_sib_hid = T.vector("Answer word's sibling hidden state")
         answer = T.scalar("Answer Probability")
 
         # Forming the processing layer
@@ -28,32 +27,29 @@ class AnsSelect(object):
             std=0.1, shape=(self.dim, self.inp_hidden_states_dim))
         self.W_ans_parent = nn_utils.normal_param(
             std=0.1, shape=(self.dim, self.inp_hidden_states_dim))
-        self.W_ans_sib = nn_utils.normal_param(
-            std=0.1, shape=(self.dim, self.inp_hidden_states_dim))
 
         self.b_inp = nn_utils.constant_param(value=0.0, shape=(self.dim))
 
         self.params = [self.W_q, self.W_ans_sent, self.W_ans_node,
-                       self.W_ans_parent, self.W_ans_sib, self.answer]
+                       self.W_ans_parent, self.answer]
 
         # Forming the output layer
         prediction = self.compute(
-            q_sent_hid, ans_sent_hid, ans_node_hid, ans_parent_hid, ans_sib_hid)
+            q_sent_hid, ans_sent_hid, ans_node_hid, ans_parent_hid)
 
         # Forming the updates and loss layer
         loss = T.sqrt(T.square(prediction) - T.square(answer))
         self.updates = lasagne.updates.adadelta(loss, self.params)
 
         self.train = theano.function([q_sent_hid, ans_sent_hid, ans_node_hid,
-                                      ans_parent_hid, ans_sib_hid, answer], [], updates=self.updates)
+                                      ans_parent_hid, answer], [], updates=self.updates)
         self.predict = theano.function(
-            [q_sent_hid, ans_sent_hid, ans_node_hid, ans_parent_hid, ans_sib_hid], prediction)
+            [q_sent_hid, ans_sent_hid, ans_node_hid, ans_parent_hid], prediction)
 
-    def compute(self, q_sent_hid, ans_sent_hid, ans_node_hid, ans_parent_hid, ans_sib_hid):
+    def compute(self, q_sent_hid, ans_sent_hid, ans_node_hid, ans_parent_hid):
         x = T.dot(self.W_q, q_sent_hid) + T.dot(self.W_ans_sent, ans_sent_hid)
             + T.dot(self.W_ans_node, ans_node_hid)
             + T.dot(self.W_ans_parent, ans_parent_hid)
-            + T.dot(self.W_ans_sib, ans_sib_hid)
 
         out = T.nnet.sigmoid(x + self.b_inp)
         return out
