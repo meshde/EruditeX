2ec4e59339ea6115d1f1454fa0f29bc43f8ad0e5
Jerin John <jerin.j@somaiya.edu>
2018-03-01 22:51:21 +0530

    Modify server for local file input.

--- edx-server.py
+++ edx-server.py
@@ -5,27 +5,23 @@ from flask import Flask
 from flask import Response
 from flask import request
 
-from Helpers import file_extraction as filer
+# from Helpers import file_extraction as filer
 
-
-def EdxServer():
+class EdXServer():
 
 	def __init__(self):
 		self.file = ''
 		self.context = []
 		self.query = '';	
-		self.app = Flask(__name__)
 
-	@self.app.route('/filed',methods=['POST'])
 	def get_file(self):
 		filename = request.get_json(force=True)['filename']
 		print(filename)
 
 		self.file = os.path.join('.\data-og\corpus\\'+filename)
 
-		self.context = filer.extract_file_contents(self.file)	
+		# self.context = filer.extract_file_contents(self.file)	
 
-	@self.app.route('/query',methods=['POST'])
 	def get_query(self):
 		
 		self.query = request.get_json(force=True)['query']
@@ -36,7 +32,19 @@ def EdxServer():
 		resp = Response(proc.communicate()[0].decode())
 		resp.headers['Access-Control-Allow-Origin'] = '*'
 		return resp
+		
+
+app = Flask(__name__)
+server = EdXServer()
+
+@app.route('/filed',methods=['POST'])
+def filer():
+	server.get_file()
+
+@app.route('/query',methods=['POST'])
+def queried():
+	server.get_query
 
 
 if __name__ == '__main__':
-	EdxServer().app.run()
+	app.run()
