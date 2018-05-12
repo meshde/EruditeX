1258699a81ef7b8ea18f34b0c93c8eee7c4d420b
meshde <meshde.md@gmail.com>
2018-05-12 15:39:13 +0530

    Add logging feature and other minor fixes

--- paper/erudite.py
+++ paper/erudite.py
@@ -7,6 +7,8 @@ from Models import abcnn_model
 from Helpers import deployment_utils as deploy
 import nltk
 import datetime
+import logging
+
 
 class EruditeX(object):
 
@@ -22,11 +24,14 @@ class EruditeX(object):
         total = 0
         dataset_size = len(babi_data_dict)
 
-        logs_file = "EruditeX_Model_Final_logs"
-        f = open(logs_file,'a')
-        z=str(datetime.datetime.now())
-        f.write(z+"\n")
-        f.write("bAbI task :"+str(babi_task_num)+"\n")
+        logs_file = "paper/logs/task_{0}_{1}".format(
+            babi_task_num,
+            str(datetime.datetime.now()),
+        )
+        logging.basicConfig(
+            filename=logs_file,
+            level=logging.INFO,
+        )
 
         for element in tqdm(babi_data_dict, total=dataset_size, unit=' Question', ncols=75):
             total += 1
@@ -37,50 +42,48 @@ class EruditeX(object):
 
             ans_dict = self.get_query(q)
 
-            print(ans_dict)
+            # print(ans_dict)
 
             ans_list = ans_dict['answers']
             predicted_answer = ans_list[0]['word']
 
-            print('predicted_answer:', predicted_answer)
-            print('actual_answer:', actual_answer)
+            # print('predicted_answer:', predicted_answer)
+            # print('actual_answer:', actual_answer)
 
-
-            if(predicted_answer is actual_answer):
+            if(predicted_answer == actual_answer):
                 count += 1
 
-            f.write("Example %d\n:"%(total))
-            f.write("Context:\n"+self.context+"\n")
-            f.write("Question: "+q+"\n")
-            f.write("Actual Answer: "+actual_answer+"\n")
-            f.write("Predicted Answer: "+predicted_answer+"\n")
-            f.write("\nTotal correct predictions as of now:%d\n"%(count))
+            logging.info("Example %d:\n"%(total))
+            logging.info("Context:\n"+self.context+"\n")
+            logging.info("Question: "+q+"\n")
+            logging.info("Actual Answer: "+actual_answer+"\n")
+            logging.info("Predicted Answer: "+predicted_answer+"\n")
+            logging.info("\nTotal correct predictions as of now:%d\n"%(count))
 
         accuracy = (count/dataset_size)*100
         print("\n Accuracy after testing on bAbI task %d is %f"%(babi_task_num,accuracy))
 
-        f.write("Accuracy after testing on bAbI task %d is %f"%(babi_task_num,accuracy))
+        logging.info("Accuracy after testing on bAbI task %d is %f"%(babi_task_num,accuracy))
 
-        f.close()
 
 
     def get_query(self, query):
         
         self.query = query
-        print('\n' + self.query)
+        # print('\n' + self.query)
         tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
         # Filter top 5 paras using Info Retrieval
         para_select = infoRX.retrieve_info(tokenizer.tokenize(self.context), self.query)
         para_sents = []
-        
 
-        print(type(para_select[0]), para_select[0])
+        # print(self.context)
+        # print(type(para_select[0]), para_select[0])
 
         for para in para_select:
             para_sents.extend(tokenizer.tokenize(para[0]))
 
-        print('Sentences selected by IR Module:')
-        print(para_sents)
+        # print('Sentences selected by IR Module:')
+        # print(para_sents)
 
         # try:
         #     # Select Ans Sents - ABCNN
@@ -103,10 +106,10 @@ class EruditeX(object):
 
         ans_sents = self.abcnn.ans_select(query, para_sents)
 
-        print('\nSystem: Sentences scored by Sentence Selection Module')
-        for sentence,score in ans_sents:
-            print('{0:50}\t{1}'.format(sentence, score[0]))
-        print('')
+        # print('\nSystem: Sentences scored by Sentence Selection Module')
+        # for sentence,score in ans_sents:
+            # print('{0:50}\t{1}'.format(sentence, score[0]))
+        # print('')
 
         best_ans, score, answers = deploy.extract_answer_from_sentences(
             ans_sents,
@@ -130,9 +133,9 @@ class EruditeX(object):
 
         ans_dict = {'answers': ans_list}
 
-        print('\nSystem: Candidate answers scored by Answer Extraction Module')
-        for answer in ans_list:
-            print('{0:10}\t{1}'.format(answer['word'], answer['score']))
+        # print('\nSystem: Candidate answers scored by Answer Extraction Module')
+        # for answer in ans_list:
+            # print('{0:10}\t{1}'.format(answer['word'], answer['score']))
 
 
         return ans_dict
